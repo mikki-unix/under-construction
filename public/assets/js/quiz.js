4cc36cfbd095d33107f34ab97d2a3f7baf8ed6b9
mikki-unix <mikki.anjos@sptech.school>
2024-11-18 08:26:05 -0300

    Sorteio das perguntas
    Consegui fazer o sistema de aleatorizar o enunciado da pergunta e aleatorizar a posição das alternativas.
    
    São duas listas "checklist" sendo sorteadas: uma para os botões e outra para o texto da alternativa.
    Quando o texto da alternativa for 0, quer dizer que essa será a correta; então é colocado "correta" ao atributo "name" do botão que foi previamente escolhido (aleatório).
    Com isso, o sistema de verificarPergunta() continua funcionando perfeitamente.
    
    Também há uma trava para que o sistema não pergunte mais do que deveria, com a função sortearPerguntas() parando.
    No entanto isso ainda tem que ser polido, com uma tela de fim de jogo a qtdAcertos...
    
    Meu próximo commit será polindo tudo isso.

--- public/assets/js/quiz.js
+++ public/assets/js/quiz.js
@@ -1,5 +1,3 @@
-var qtdAcertos = 0;
-
 const perguntas = [
     {
         enunciado: 'Quem foi gege..?',
@@ -11,29 +9,87 @@ const perguntas = [
     }
 ]
 
-function sortearPergunta() {
-    /* 
-        Precisa de uma forma para aleatorizar a alternativa correta e sua posição, além de saber quais perguntas já foram feitas (pensei numa espécie de checklist).
-
-        Depois terá um for que também sorteia onde colocará todas as alternativas e dará "names" para elas...
-    */
+function comecarQuiz() {
+    criarCheckListPerguntas()
+    sortearPergunta()
 }
 
+const checkListPerguntas = []
+function criarCheckListPerguntas() {
+    for (var numero = 0; numero < perguntas.length; numero++) {
+        checkListPerguntas.push(numero)
+    }
+}
+
+var numeroPergunta = 0
+function sortearPergunta() {
+    if (checkListPerguntas.length == 0) {
+        return alert('cabo')
+    }
+
+    const posicaoPergunta = parseInt(Math.random() * checkListPerguntas.length)
+
+    const enunciadoPergunta = perguntas[checkListPerguntas[posicaoPergunta]].enunciado
+
+    checkListPerguntas.splice(posicaoPergunta, 1)
+
+    span_enunciado.innerText = enunciadoPergunta
+    numeroPergunta++
+    strong_numero_pergunta.innerText = numeroPergunta + '.'
+
+    sortearAlternativas(posicaoPergunta)
+}
+
+function sortearAlternativas(posicaoPergunta) {
+    const checkListSpan = [0, 1, 2]
+    const checklistTextoAlternativa = [0, 1, 2]
+
+    for (
+        var numeroAlternativa = 0;
+        numeroAlternativa <= 2;
+        numeroAlternativa++ 
+    ) {
+        const numeroAleatorioSpan = parseInt(Math.random() * checkListSpan.length)
+        
+        const spanSelecionado = document.getElementById(`span_texto_alternativa${checkListSpan[numeroAleatorioSpan]}`)
+        const btnSelecionado = document.getElementById(`btn_alternativa${checkListSpan[numeroAleatorioSpan]}`)
+        
+        checkListSpan.splice(numeroAleatorioSpan, 1)
+
+        const numeroAleatorioTexto = parseInt(Math.random() * checklistTextoAlternativa.length)
+        spanSelecionado.innerText = perguntas[posicaoPergunta].alternativas[checklistTextoAlternativa[numeroAleatorioTexto]]
+        
+        if (checklistTextoAlternativa[numeroAleatorioTexto] == 0) {
+            btnSelecionado.setAttribute('name', 'correta')
+        } else {
+            btnSelecionado.setAttribute('name', 'incorreta')
+        }
+
+        checklistTextoAlternativa.splice(numeroAleatorioTexto, 1)
+    }
+}
+
+var qtdAcertos = 0;
+var qtdErros = 0;
 function verificarResposta(numero = '') {
     const alternativa = document.getElementById(`btn_alternativa${numero}`)
 
     const nameAlternativa = alternativa.getAttribute('name')
     const imgAlternativa = document.getElementById(`img_alternativa${numero}`)
 
-    if(nameAlternativa == 'correta') {
+    if (nameAlternativa == 'correta') {
         alternativa.classList.toggle(nameAlternativa)
         imgAlternativa.setAttribute('src', 'assets/icons/check.svg')
+
+        qtdAcertos++
     } else {
         alternativa.classList.toggle(nameAlternativa)
         imgAlternativa.setAttribute('src', 'assets/icons/cross.svg')
+
+        qtdErros++
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         alternativa.classList.toggle(nameAlternativa)
         imgAlternativa.removeAttribute('src')
 
